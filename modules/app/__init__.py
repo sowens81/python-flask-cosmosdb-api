737d55507466b24f49d36dda294dc5516c20d68d
steve owens <steve.owens@uk.fujitsu.com>
2019-10-15 22:15:09 +0000

    fixed code

--- modules/app/__init__.py
+++ modules/app/__init__.py
@@ -1,9 +1,33 @@
-""" Flask app with mongo """
-
+''' flask app with mongo '''
 import os
 import json
 import datetime
+from bson.objectid import ObjectId
 from flask import Flask
+from flask_pymongo import PyMongo
 
-#Create the flask object
+
+
+class JSONEncoder(json.JSONEncoder):
+    ''' extend json-encoder class'''
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, set):
+            return list(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+# create the flask object
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ.get('DB')
+mongo = PyMongo(app)
+
+
+
+app.json_encoder = JSONEncoder
+
+from app.controllers import *  # pylint: disable=W0401,C0413
